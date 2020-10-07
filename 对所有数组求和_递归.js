a3ac90c4d2fc3854ae41c6346e99b77c2d0f6f6b
tiluzhanshi <hi_beijing@126.com>
2020-10-07 16:57:56 +0800

    对所有数组求和_递归

---
+++ 对所有数组求和_递归.js
@@ -0,0 +1,15 @@
+var array = [1,2,3,4,5,6];
+/**
+对array所有元素求和
+找重复（子问题）：
+找变化：找重复中的变化量->参数
+找边界：找出参数变化趋势->设计出口
+*/
+function sum(array, n) {
+  if(n==array.length-1) {
+    return array[n];
+  }
+  return array[n] + sum(array, ++n);
+}
+
+sum(array, 0);
